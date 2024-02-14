a98dd3fd71959a61f24045db10ffa3a3a15ddb35
Sylvain Vandermeersch <sylvain.vandermeersch@gmail.com>
2024-02-14 01:36:51 +0100

    clearing code

--- config/Command/FixturesLoad.php
+++ config/Command/FixturesLoad.php
@@ -1,7 +1,6 @@
 <?php
 
 namespace Core\Command;
-// require_once __DIR__ . '/../../vendor/autoload.php';
 
 use App\Fixtures\CommentFixtures;
 use App\Fixtures\PostFixtures;
@@ -14,21 +13,21 @@ $consoleIO = new ConsoleIO();
 
 for ($i = 0; $i < 10; $i++) {
   try {
-    $consoleIO->writeLine('start load a new user');
+    $consoleIO->writeLine('Starting to load a new user');
     $userFixtures = new UserFixtures();
     $userIdGenerated = $userFixtures->load();
-    $consoleIO->writeLine(sprintf('new user generated and loaded in database with id: %s', $userIdGenerated));
+    $consoleIO->writeLine(sprintf('New user generated and loaded in database with id: %s', $userIdGenerated));
   } catch (Exception $e) {
-    echo "Erreur lors de l'exécution de UserFixtures : " . $e->getMessage() . "\n";
+    echo "Error during the execution of UserFixtures : " . $e->getMessage() . "\n";
   }
 }
 
 for ($i = 0; $i < 15; $i++) {
   try {
-    $consoleIO->writeLine('start loading new post');
+    $consoleIO->writeLine('Starting to load  a new post');
     $postFixtures = new PostFixtures();
     $postIdGenerated = $postFixtures->load();
-    $consoleIO->writeLine(sprintf('new post generated and loaded in database with id: %s', $postIdGenerated));
+    $consoleIO->writeLine(sprintf('New post generated and loaded in database with id: %s', $postIdGenerated));
   } catch (Exception $e) {
     echo "Error during the execution of PostFixtures" . $e->getMessage() . "\n";
   }
@@ -36,15 +35,13 @@ for ($i = 0; $i < 15; $i++) {
 
 for ($i = 0; $i < 15; $i++) {
   try {
-    $consoleIO->writeLine('start load new comment');
+    $consoleIO->writeLine('Starting to load a new comment');
       $commentFixtures = new CommentFixtures();
       $commentIdGenerated = $commentFixtures->load();
-      $consoleIO->writeLine(sprintf('new comment generated and loaded in database with id: %s', $commentIdGenerated));
+      $consoleIO->writeLine(sprintf('New comment generated and loaded in database with id: %s', $commentIdGenerated));
   } catch (Exception $e) {
-      echo "Erreur lors de l'exécution de CommentFixtures : " . $e->getMessage() . "\n";
+      echo "Error during the execution CommentFixtures : " . $e->getMessage() . "\n";
   }
 }
 
-echo " Exécution des Fixtures terminée.\n";
-
-// try to add a audio file for winning
+echo " Execution of fixtures finished.\n";
